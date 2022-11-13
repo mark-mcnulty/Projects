a99bc1d14e8bfdfca68e88bebbd382cdb56dd675
Mark McNulty <M.MCNULTYMM2@GMAIL.COM>
2022-11-13 12:48:20 -0700

    idk

--- spi/spi_two_arduino/test/spiMaster/spiMaster.ino
+++ spi/spi_two_arduino/test/spiMaster/spiMaster.ino
@@ -15,21 +15,29 @@
 void setup(void) {
   Serial.begin(115200);
 
-  // setup SPI master
-  SPI.begin();
-
   // The slave select pin is active low so we need to set it high to disable the slave
   digitalWrite(CS_PIN, HIGH);
 
+  // setup SPI master
+  SPI.begin();
+
   // // sets the clk to a division of the system cloc 
-  // SPI.setClockDivider()
+  SPI.setClockDivider(SPI_CLOCK_DIV8);
 }
 
-
-// set the master receive and transmit bytes
-byte masterSend;
-byte masterReceive;
+char c;
 
 void loop(void) {
+  // activate the slave
+  digitalWrite(CS_PIN, LOW);
 
+  // send a string 
+  for(constant char* p = "Hello World"; *p; p++) {
+    SPI.transfer(p);
+    Serial.print(p);
+  }
+
+  // disable the slave
+  digitalWrite(CS_PIN, HIGH);
+  delay(3000);
 }
